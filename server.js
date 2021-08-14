f4978595ad533b5d73a4c914dfff3b5f9f6fc013
siid sharma <siid123ck@gmail.com>
2021-08-14 15:45:04 +1000

    updated the database file

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
-const express=require('express')
+const express=require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const dotenv = require('dotenv')
 
-const app = express(); 
+const MongoStore = require('connect-mongo')(session);
 
-const middleware1 = (req, res, next)=>{
-    console.log('This is middleware 1'); 
-    next()
+
+const app = express();  
+
+ 
+dotenv.config();
+const db = process.env.LOCAL_DATABASE; 
+const dbOptions = {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
 }
+
+
+mongoose.connect(db, dbOptions)
+const dbConnection = mongoose.connection;
+
+dbConnection.on('error', ()=>console.log(error)); 
+dbConnection.once('open', ()=>console.log('database connected'))
+
+const sessionStore = new MongoStore({
+mongooseConnection: dbConnection, 
+collection:'sessions'
+})
+
+app.use(session({
+    secret:"something", 
+    resave:false,
+    saveUninitialized:true, 
+    store:sessionStore,
+    cookie:{
+        maxAge:1000*60*60*24
+    }
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
 const notFoundiddleware = (req, res, next)=>{
     console.log('This is not found middleware '); 
     const errObj = new Error('This rout is not defined')
@@ -16,11 +52,11 @@ const errorHandler = (err, req, res, next)=>{
     res.send(`<h1>${err}</h1>`)
 }
 
-app.use(middleware1);
 app.get('/', (req, res)=>{
     res.end('This is express application')
 })
  
 app.use(notFoundiddleware); 
 app.use(errorHandler)
-app.listen(4000, ()=>console.log('App is running on server')) 
+const PORT = process.env.PORT
+app.listen(4000, ()=>console.log(`App is running on server ${PORT}`)) 
