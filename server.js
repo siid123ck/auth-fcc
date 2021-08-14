8ffffd9f17801f505601599c60c49fd12ce3a1b4
siid sharma <siid123ck@gmail.com>
2021-08-14 17:34:29 +1000

    basic session complete

--- server.js
+++ server.js
@@ -1,20 +1,55 @@
 const express=require('express');
-const errorHandler = require('./middleware/errorHandler');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const { notFoundiddleware } = require('./middleware/notFound');
-require('./database'); 
+const errorHandler = require('./middleware/errorHandler'); 
+
+const MongoStore = require('connect-mongo')(session);
 
 
 const app = express();  
+
+ 
+dotenv.config();
+const db = process.env.LOCAL_DATABASE; 
+const dbOptions = {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
+
+
+mongoose.connect(db, dbOptions)
+const dbConnection = mongoose.connection;
+
+dbConnection.on('error', ()=>console.log(error)); 
+dbConnection.once('open', ()=>console.log('database connected'))
+
+const sessionStore = new MongoStore({
+mongooseConnection: dbConnection, 
+collection:'sessions'
+})
+
+app.use(session({
+    secret:"something", 
+    resave:false,
+    saveUninitialized:true, 
+    store:sessionStore,
+    cookie:{
+        maxAge:1000*60*60*24
+    }
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
 
-
-app.get('/session', (req, res)=>{
+app.get('/', (req, res)=>{
     console.log(req.session)
-    res.send('<h1>This is session application<h1>') 
+    req.session.viewCount ? req.session.viewCount = req.session.viewCount+1 : req.session.viewCount=1;
+    res.send(`<h1>You visited  express application ${req.session.viewCount} times</h1>`)
 })
+ 
  
 app.use(notFoundiddleware); 
 app.use(errorHandler)
